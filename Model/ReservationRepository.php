c163d0db853ea8dd1c2076acf4311d5d5fd7c9ec
Guilherme <86965518+guilhermemap@users.noreply.github.com>
2025-07-14 12:28:30 -0300

    fix == -> != on previous commit and fix to join also on sku
    if an order had more than one sku, it would wrongly appear in the join

--- Model/ReservationRepository.php
+++ Model/ReservationRepository.php
@@ -31,8 +31,8 @@ class ReservationRepository implements ReservationRepositoryInterface
         $result = $connection->query("select r.* from inventory_reservation r
         left join inventory_reservation r2 on
             JSON_EXTRACT(r.metadata,'$.object_increment_id')=JSON_UNQUOTE(JSON_EXTRACT(r2.metadata,'$.object_increment_id'))
-            and r.reservation_id!=r2.reservation_id
-        where (r2.reservation_id is null or r.quantity=r2.quantity*-1)
+            and r.reservation_id!=r2.reservation_id and r.sku=r2.sku
+        where (r2.reservation_id is null or r.quantity!=r2.quantity*-1)
             and r.sku=?;", [$sku])->fetchAll();
 
 
